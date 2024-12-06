f54a79b93e6c2dbf1e9356bca3716c6527c96d4e
sparklingp8 <mynewnokiap8@gmail.com>
2024-12-06 15:00:21 +0000

    made more time

--- timeCounter/views.py
+++ timeCounter/views.py
@@ -9,17 +9,33 @@ def getAge(request):
     timers = [
         {
             'event_name': 'Dreams',
-            'target_time': '2025-06-06 19:43:46'
+            'target_time': '2027-03-07 11:46:46'
+        },
+        {
+            'event_name': 'Mid',
+            'target_time': '2040-02-07 23:56:00'
         },
         {
             'event_name': 'Life',
-            'target_time': '2060-01-01 00:00:00'
-        }
+            'target_time': '2067-02-07 23:56:00'
+        },
+        {
+            'event_name': 'Python Master',
+            'target_time': '2025-02-26 23:58:02'
+        },
+        # {
+        #     'event_name': 'USA',
+        #     'target_time': '2025-02-26 23:58:00'
+        # },
     ]
 
+    # Sort the timers list by 'target_time' after converting the string to a datetime object
+    timers_sorted = sorted(timers, key=lambda x: datetime.strptime(x['target_time'], '%Y-%m-%d %H:%M:%S'))
+
+    # Prepare the context
     context = {
-        'timers': timers,
-        'timers_json': json.dumps(timers)  # Pass JSON-serialized data to the template
+        'timers': timers_sorted,
+        'timers_json': json.dumps(timers_sorted)  # Pass JSON-serialized data to the template
     }
 
     return render(request, 'timeCounter/time.html', context)
