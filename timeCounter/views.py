4f4a6f0285e3fd75c854528a75fb300c60d75ee3
sparklingp8 <mynewnokiap8@gmail.com>
2024-12-06 11:40:02 +0000

    added css

--- timeCounter/views.py
+++ timeCounter/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 import time
-def getAge(request):
-    theTime = time.strftime("%d/%m/%Y %H:%M:%S")
+from datetime import datetime, timedelta
 
-    return render(request, "timeCounter/time.html", {"theTime":theTime})
+
+def getAge(request):
+    theTime = datetime(2030, 11, 8)
+    theTime = datetime.now() + timedelta(minutes=1)
+    print(theTime)
+    important_dates = [
+    {"date": "2070-03-07", "label": "Last"},
+    {"date": "2030-03-07", "label": "Dream"},
+    # Add more dates as needed
+]
+
+    return render(request, "timeCounter/time.html", {"important_dates":important_dates})
