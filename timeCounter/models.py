ff9b3f87e63da269ed7064c76b1ca40e4c4228b8
sparklingp8 <mynenokiap8@gmail.com>
2024-12-10 12:46:26 +0530

    intial show_hide box

--- timeCounter/models.py
+++ timeCounter/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    event_name = models.CharField(max_length=255)
+    target_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.event_name} - {self.target_time}"
+
+    class Meta:
+        verbose_name = 'Event'
+        verbose_name_plural = 'Events'
+        ordering = ['target_time']
