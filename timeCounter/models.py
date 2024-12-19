2aba8295bc73cb771346090bd42b64042241243e
sparklingp8 <mynewnokiap8@gmail.com>
2024-12-19 16:59:44 +0530

    password

--- timeCounter/models.py
+++ timeCounter/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Event(models.Model):
     creator_id = models.CharField(max_length=255)
     data = models.JSONField(default=dict)
+    creator_pin =  models.IntegerField(default=0, null=False, blank=False) 
     
     def __str__(self):
         return f"Event for Creator {self.creator_id}, Countdowns List: {self.data["events"]}"
