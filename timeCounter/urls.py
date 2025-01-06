f1a42ad71d1591be554be0dce5cae371a81a059b
sparklingp8 <mynewnokiap8@gmail.com>
2025-01-06 13:05:22 +0530

    added register user front end complete

--- timeCounter/urls.py
+++ timeCounter/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     path("age", views.getAge, name="get_age"),
-    path("addNewUser",views.add_new_user, name="add_new_user"),
+    path("addNewUser",views.add_new_user, name="add_new_user"), 
+    path('check-username/<str:username>', views.check_username, name='check-username'),
     path("<str:userID>", views.user_countdown, name="user_countdowns" ),    
     path("addCountdown/<str:userID>", views.user_add_countdown, name="user_add_countdowns" ),
     path("countdowns/delCountdown/<str:userID>/<str:count_name>", views.user_delete_countdown, name="user_delete_countdowns" ),
